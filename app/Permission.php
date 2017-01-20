7e6f7d0fae0bc959c4ca5fdb2e0e1696b3db576e
galinovivan <9225913@gmail.com>
2017-01-20 23:13:58 +0300

    inita;

---
+++ app/Permission.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ann
+ * Date: 16.01.2017
+ * Time: 22:40
+ */
+
+namespace App;
+
+use Zizaco\Entrust\EntrustPermission;
+
+class Permission extends EntrustPermission
+{
+
+}
---
+++ app/Role.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ann
+ * Date: 16.01.2017
+ * Time: 22:38
+ */
+
+namespace App;
+
+use Zizaco\Entrust\EntrustRole;
+
+class Role extends EntrustRole
+{
+
+}
